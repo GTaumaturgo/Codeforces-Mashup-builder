9c408ee1eb8e3cf6fc7600101a9dd3e35e2475fa
gtaumaturgo <gtaumaturgo@github.com>
2019-06-08 23:29:44 -0300

    improved the crawler so that now it cant ignore the recently aced problems for a list of users!

--- mashup_builder.py
+++ mashup_builder.py
@@ -1,14 +1,38 @@
 import requests
 import json
 import random
- 
-url = "http://codeforces.com/api/problemset.problems"
-contents = requests.get(url).json()
-prob_list = list(filter(lambda d : 'rating' in d, contents['result']['problems']))
+import time
+url_problems = "http://codeforces.com/api/problemset.problems"
+url_submissions = "http://codeforces.com/api/user.status?handle={}&count={}"
 
-print("Por favor digite quantos problemas você quer no mashup, \n dê enter, depois entre um número de dificuldades igual ao número de problemas.")
-num_problems = int(input())
-dif_list = map(int, input().split())
+def getACsByUser(handle, cnt):
+    while True:
+        ans = requests.get('http://codeforces.com/api/user.status?handle={}&count={}'.format(handle, cnt)).json()
+        if ans['status'] == 'OK': break
+        time.sleep(0.3)
+    return [(str(submition['contestId']) + str(submition['problem']['index'])) for submition in ans['result'] if submition['verdict'] == 'OK']
+
+print("Fetching problems...")
+prob_list = list(filter(lambda d : 'rating' in d, requests.get(url_problems).json()['result']['problems']))
+
+handle_list = []
+dif_list = []
+with open("handles") as file :
+    for line in file :
+        handle_list.append(line.strip())
+with open("difficulties") as file :
+    for line in file :
+        dif_list.append(int(line.strip()))
+
+
+ACed_problems = set()
+
+print("Fetching handles...")
+for handle in handle_list :
+    print(handle)
+    ACed_problems.update(set(getACsByUser(handle, 400)))
+
+
 
 dificulties_dict = {}
 for prob in prob_list:
@@ -21,17 +45,18 @@ for prob in prob_list:
 for key in dificulties_dict :
     print("Na dificuldade %d tem %d problemas!" % (key, len(dificulties_dict[key])))
 
+print("Fetching problems not aced by any user...")
 taken = set()
 for dif in dif_list :
     while(True):
-        try : 
-            ind = random.randint(0, len(dificulties_dict[dif]) - 1)
-        except :
+        if dif not in dificulties_dict :
             print("alguma das dificuldades nao tem nenhum problema nela! encerrando!")
             exit(0)
-        if dificulties_dict[dif][ind] not in taken :
+        ind = random.randint(0, len(dificulties_dict[dif]) - 1)
+
+        if dificulties_dict[dif][ind] not in taken and dificulties_dict[dif][ind] not in ACed_problems:
             taken.add(dificulties_dict[dif][ind])
             break
-
+print("Results!")
 for problem in taken :
     print(problem)
