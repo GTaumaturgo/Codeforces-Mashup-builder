62a5ade70a408190ba9bfcd0936d278ecb51469e
Maxwell Oliveira <maxwell1de1oliveira@gmail.com>
2021-09-22 21:09:23 -0300

    add search by tags and some optimizations

--- mashup_builder.py
+++ mashup_builder.py
@@ -16,33 +16,57 @@ def getACsByUser(handle, cnt):
     except :
         return []
         
-
+def notFoundSomething():
+    print("alguma das dificuldades nao tem nenhum problema nela! encerrando!")
+    exit(0)
 
 
 print("Fetching problems...")
 prob_list = list(filter(lambda d : 'rating' in d, requests.get(url_problems).json()['result']['problems']))
 
 handle_list = []
-dif_list = []
+dif_set = set()
+dif_count = {}
+tags = []
+
+with open("tags") as file:
+    for line in file:
+        tags.append(line.strip())
 with open("handles") as file :
     for line in file :
         handle_list.append(line.strip())
 with open("difficulties") as file :
     for line in file :
-        dif_list.append(int(line.strip()))
-
+        dif = int(line.strip())
+        dif_set.add(dif)
+        if dif in dif_count:
+            dif_count[dif] += 1
+        else:
+            dif_count[dif] = 1
 
 ACed_problems = set()
 
 print("Fetching handles...")
 for handle in handle_list :
     print(handle)
-    ACed_problems.update(getACsByUser(handle, 400))
+    ACed_problems.update(getACsByUser(handle, 1000))
 
 
 
 dificulties_dict = {}
 for prob in prob_list:
+
+    if str(prob['contestId'])+prob['index'] in ACed_problems:
+        continue
+
+    tagNotFound = 0
+    for tag in tags:
+        if tag not in prob['tags']:
+            tagNotFound = 1
+            break
+    
+    if tagNotFound: continue
+
     rat = int(prob['rating'])
     if rat in dificulties_dict :
         dificulties_dict[rat].append(str(prob['contestId']) + prob['index'])
@@ -54,16 +78,30 @@ for key in dificulties_dict :
 
 print("Fetching problems not Aced by any user...")
 taken = set()
-for dif in dif_list :
-    while(True):
-        if dif not in dificulties_dict :
-            print("alguma das dificuldades nao tem nenhum problema nela! encerrando!")
-            exit(0)
-        ind = random.randint(0, len(dificulties_dict[dif]) - 1)
+for dif in dif_set :
 
-        if (dif, dificulties_dict[dif][ind]) not in taken and dificulties_dict[dif][ind] not in ACed_problems:
-            taken.add((dif ,(dificulties_dict[dif][ind])))
-            break
+    if dif not in dificulties_dict:
+        print(dif)
+        notFoundSomething()
+
+    if len(dificulties_dict[dif]) < dif_count[dif]:
+        print(dif)
+        notFoundSomething()
+    
+    elif len(dificulties_dict[dif]) == dif_count[dif]:
+        #random is useless here
+        for prob in dificulties_dict[dif]:
+            taken.add((dif,(prob)))
+    
+    else:
+        for i in range(dif_count[dif]):
+            while(True):
+
+                ind = random.randint(0, len(dificulties_dict[dif]) - 1)
+                
+                if (dif, dificulties_dict[dif][ind]) not in taken and dificulties_dict[dif][ind] not in ACed_problems:
+                    taken.add((dif ,(dificulties_dict[dif][ind])))
+                    break
 
 print("Results!")
 l = list(taken)
